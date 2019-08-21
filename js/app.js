64decdccd3ae8017f87e00bb50a2bbeca553b9ff
Avrey125 <52896319+Avrey125@users.noreply.github.com>
2019-08-20 23:48:56 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,152 @@
+'use strict'
+
+var imageElement1 = document.querySelector('#image1');
+var imageElement2 = document.querySelector('#image2');
+var imageElement3 = document.querySelector('#image3');
+var imageCaption1 = document.querySelector('#caption1');
+var imageCaption2 = document.querySelector('#caption2');
+var imageCaption3 = document.querySelector('#caption3');
+
+
+var countButtonClicks = 1;
+
+function Image(name,url, timesShown) {
+    this.name = name;
+    this.src = url;
+    this.timesShown = timesShown;
+    Image.list.push(this);
+}
+
+Image.list = [];
+// while (imageElement1 === imageElement2 || imageElement1 === imageElement3 || imageElement2 === imageElement3 || images.lastlastShown.includes(imageElement1));
+
+function loadImages() {
+    new Image('("Robot") Bag','/images/bag.jpg', 0);
+    new Image('Bathroom Phone Holder','/images/bathroom.jpg', 0);
+    new Image('Breakfast Maker','/images/breakfast.jpg', 0);
+    new Image('Meatball Bubblegum','/images/bubblegum.jpg', 0);
+    new Image('Chair','/images/chair.jpg', 0);
+    new Image('Cthulhu','/images/cthulhu.jpg', 0);
+    new Image('Duck Mask','/images/dog-duck.jpg', 0);
+    new Image('Dragon','/images/dragon.jpg', 0);
+    new Image('U-Pensils','/images/pen.jpg', 0);
+    new Image('Pet Sweep','/images/pet-sweep.jpg', 0);
+    new Image('Pizza Scissors','/images/scissors.jpg', 0);
+    new Image('Shark Blanket','/images/shark.jpg', 0);
+    new Image('Baby Sweep','/images/sweep.png', 0);
+    new Image('TaunTaun Sleepingbag','/images/tauntaun.jpg', 0);
+    new Image('Unicorn Meat','/images/unicorn.jpg', 0);
+    new Image('USB','/images/usb.gif', 0);
+    new Image('Water Can','/images/water-can.jpg', 0);
+    new Image('Wine Glass','/images/wine-glass.jpg', 0);
+    new Image('boots', '/images/boots.jpg', 0);
+    new Image('banana','/images/banana.jpg', 0);
+    };
+    
+
+    
+loadImages();
+
+function showImage() {
+    var randomNumber1 = Math.floor(Math.random() * Image.list.length); 
+    
+    var randomNumber2 = Math.floor(Math.random() * Image.list.length); 
+
+    var randomNumber3 = Math.floor(Math.random() * Image.list.length); 
+
+    //image 1
+    imageElement1.src = Image.list[randomNumber1].src;
+    Image.list[randomNumber1].timesShown ++;
+    caption1.textContent = Image.list[randomNumber1].name;
+    
+    //image 2
+    imageElement2.src = Image.list[randomNumber2].src;
+    Image.list[randomNumber2].timesShown ++;
+    caption2.textContent = Image.list[randomNumber2].name; 
+    
+    //image 3
+    imageElement3.src = Image.list[randomNumber3].src;
+    Image.list[randomNumber3].timesShown ++;
+    caption3.textContent = Image.list[randomNumber3].name;
+    
+}
+imageElement1.src = Image.list[1].src;
+imageElement2.src = Image.list[2].src;
+imageElement3.src = Image.list[3].src;
+caption1.textContent = Image.list[1].name;
+caption2.textContent = Image.list[2].name;    
+caption3.textContent = Image.list[3].name;
+
+
+    
+imageElement1.addEventListener('click', function(){
+        if(countButtonClicks < 26) {
+    countButtonClicks++;
+    showImage();
+    
+    }  else {
+        makeChart(data, labels);
+    }
+});
+
+imageElement2.addEventListener('click', function(){
+    if(countButtonClicks < 26) {
+    countButtonClicks++; 
+    showImage(); 
+    
+    } else {
+        makeChart(data, labels);
+    }
+});
+
+imageElement3.addEventListener('click', function() {
+    if(countButtonClicks < 26) {
+    countButtonClicks++;
+    showImage(); 
+    }  else {
+        makeChart(data, labels);
+    } 
+});
+console.log(countButtonClicks)
+
+var ctx = document.getElementById('chart').getContext('2d');
+var labels = [];
+var data = [];
+
+
+// makeChart(data, labels);
+
+function makeChart(data, labels) {
+    
+    for (var i = 0; i < Image.list.length; i++) {
+        labels.push(Image.list[i].name);
+        data.push(Image.list[i].timesShown);
+        
+    }
+
+            var ctx = document.getElementById('chart').getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'bar',
+
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: '# of Clicks',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: data ,
+                }]
+            },
+        
+            // Configuration options go here
+            options: {}
+        });
+        }
+
+
+
+
+
+
+
+
